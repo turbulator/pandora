99afb869b58070725e8b21bcc20e12912f4b93cc
Alexey Vlasov <mail4turbo@gmail.com>
2020-05-05 13:31:23 +0700

    Add new binary sensors and do refactor for existing

--- binary_sensor.py
+++ binary_sensor.py
@@ -15,12 +15,18 @@ DEPENDENCIES = ['pandora']
 
 _LOGGER = logging.getLogger(__name__)
 
-SENSOR_TYPES = {
-    'engine_state': ['engine state', 'moving'],
-    'connection_state': ['connection state', 'connectivity'],
-}
 
-#SENSOR_TYPES_ELEC.update(SENSOR_TYPES)
+SENSOR_TYPES = {
+    'connection_state': ["Connection state", "", "",  'connectivity', False, "online", 0x00000001, 0x00000000],
+    'engine_state': ["Engine state", "mdi:fan", "mdi:fan-off", "", True, "bit_state_1", 0x00000004, 0x00000000],
+    'lock_state': ["Lock", "mdi:lock-open", "mdi:lock", "lock", True, "bit_state_1", 0x00000001, 0x00000001],
+    'left_front_door': ["Left Front_Door", "mdi:car-door", "mdi:car-door", "door", True, "bit_state_1", 0x0020000, 0x00000000],
+    'right_front_door': ["Right Front Door", "mdi:car-door", "mdi:car-door", "door", True, "bit_state_1", 0x0040000, 0x00000000],
+    'left_back_door': ["Left Back Door", "mdi:car-door", "mdi:car-door", "door", True, "bit_state_1", 0x00800000, 0x00000000],
+    'right_back_door': ["Right Back Door", "mdi:car-door", "mdi:car-door", "door", True, "bit_state_1", 0x0100000, 0x00000000],
+    'trunk': ["Trunk", "mdi:car-back", "mdi:car-back", "door", True, "bit_state_1", 0x0200000, 0x00000000],
+    'hood': ["Hood", "mdi:car", "mdi:car", "door", True, "bit_state_1", 0x0400000, 0x00000000],
+}
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
@@ -31,27 +37,47 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     devices = []
     for account in accounts:
         for vehicle in account.account.vehicles:
-#            _LOGGER.debug('BMW with a high voltage battery')
-            for key, value in sorted(SENSOR_TYPES.items()):
-                device = PandoraSensor(account, vehicle, key,
-                                                 value[0], value[1])
+            for parameter, _ in sorted(SENSOR_TYPES.items()):
+                name = SENSOR_TYPES[parameter][0]
+                icon_on = SENSOR_TYPES[parameter][1]
+                icon_off = SENSOR_TYPES[parameter][2]
+                device_class = SENSOR_TYPES[parameter][3]
+                state_sensitive = SENSOR_TYPES[parameter][4]
+                attribute = SENSOR_TYPES[parameter][5]
+                bit_mask = SENSOR_TYPES[parameter][6]
+                xor_mask = SENSOR_TYPES[parameter][7]
+
+                device = PandoraSensor(account, 
+                                       vehicle,
+                                       name,
+                                       icon_on,
+                                       icon_off,
+                                       device_class,
+                                       attribute,
+                                       bit_mask,
+                                       xor_mask,
+                                       state_sensitive)
                 devices.append(device)
+
     add_entities(devices, True)
 
 
 class PandoraSensor(BinarySensorDevice):
     """Representation of a BMW vehicle binary sensor."""
 
-    def __init__(self, account, vehicle, attribute: str, sensor_name,
-                 device_class):
+    def __init__(self, account, vehicle, name: str, icon_on: str, icon_off: str, device_class: str, attribute: str, bit_mask: int, xor_mask: int, state_sensitive: bool):
         """Constructor."""
-        self._account = account
         self._vehicle = vehicle
-        self._attribute = attribute
-        self._name = '{} {}'.format(self._vehicle.name, sensor_name)
-        self._sensor_name = sensor_name
-        self._device_class = device_class
+        self._account = account
+        self._name = '{} {}'.format(self._vehicle.name, name)
         self._state = None
+        self._icon_on = icon_on
+        self._icon_off = icon_off
+        self._device_class = device_class
+        self._attribute = attribute
+        self._bit_mask = bit_mask
+        self._xor_mask = xor_mask
+        self._state_sensitive = state_sensitive
 
     @property
     def should_poll(self) -> bool:
@@ -62,57 +88,47 @@ class PandoraSensor(BinarySensorDevice):
         return False
 
     @property
-    def name(self):
+    def name(self) -> str:
         """Return the name of the binary sensor."""
         return self._name
 
     @property
-    def device_class(self):
+    def device_class(self) -> str:
         """Return the class of the binary sensor."""
         return self._device_class
 
     @property
-    def is_on(self):
+    def icon(self) -> str:
+        """Return the icon of the binary sensor."""
+        if self._state:
+            return self._icon_on
+        else:
+            return self._icon_off
+
+    @property
+    def is_on(self) -> bool:
         """Return the state of the binary sensor."""
         return self._state
 
     @property
     def device_state_attributes(self):
-        """Return the state attributes of the binary sensor."""
-        vehicle_state = self._vehicle.state
-        result = {
+        """Return the state attributes of the sensor."""
+        return {
             'car': self._vehicle.name
         }
 
-        if self._attribute == 'engine_state':
-            if vehicle_state.bit_state_1 & 0x00000004:
-                result['engine_state'] = 'running'
-            else:
-                result['engine_state'] = 'stopped'
-        elif self._attribute == 'connection_state':
-            if vehicle_state.online == 1:
-                result['connection state'] = 'online'
-            else:
-                result['connection state'] = 'offline'
-
-        return sorted(result.items())
-
-
     def update(self):
-#        """Read new state data from the library."""
+        """Read new state data from the library."""
         vehicle_state = self._vehicle.state
 
-        if self._attribute == 'engine_state':
-            if vehicle_state.bit_state_1 & 0x00000004:
+        if vehicle_state.online == 1 or self._state_sensitive == False:
+            self._available = True
+            if getattr(vehicle_state, self._attribute) & self._bit_mask ^ self._xor_mask:
                 self._state = True
             else:
                 self._state = False
-        elif self._attribute == 'connection_state':
-            if vehicle_state.online == 1:
-                self._state = True
-            else:
-                self._state = False
-
+        else:
+            self._available = False
 
     def update_callback(self):
         """Schedule a state update."""
